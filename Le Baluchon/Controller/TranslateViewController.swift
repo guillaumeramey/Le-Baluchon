22eee92c16dd18de186af63534d72b1ffc98e293
Guillaume Ramey <guillaume.ramey@gmail.com>
2018-12-13 17:47:16 +0700

    Project creation Tab bar controller & principal views created

---
+++ Le Baluchon/Controller/TranslateViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class TranslateViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
